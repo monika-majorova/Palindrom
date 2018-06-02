43e444cc2e03adb0fd5064a509f6a73b58154a91
monika-majorova <39841608+monika-majorova@users.noreply.github.com>
2018-06-02 23:29:33 +0200

    Změna názvu proměnné isPalindrome

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var QUnit = require('qunitjs'); // Načíst modul QUnit node.js module pro testování.
 var test = QUnit.test; // Uložit kopii QUnit.test.
 require('qunit-tap')(QUnit, console.log); // Použít console.log pro výstup testu.
-var isPalindrom = require('./palindrom.js'); // Načást soubor palindrom.js.
+var isPalindrome = require('./palindrom.js'); // Načást soubor palindrom.js.
 
 test('Testování funkce vyhodnocující, zda jde o palindrom', function(assert) {
   var input = "aaa";
