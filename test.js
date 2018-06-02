49d7e00296da1fb7d2b03887e8d3b4b7acf6814a
monika-majorova <39841608+monika-majorova@users.noreply.github.com>
2018-06-02 22:55:11 +0200

    Doplnění testu

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
+var QUnit = require('qunitjs'); // Načíst modul QUnit node.js module pro testování.
+var test = QUnit.test; // Uložit kopii QUnit.test.
+require('qunit-tap')(QUnit, console.log); // Použít console.log pro výstup testu.
+var isPalindrom = require('./palindrom.js'); // Načást soubor palindrom.js.
+
 test('Testování funkce vyhodnocující, zda jde o palindrom', function(assert) {
   var input = "aaa";
   var result = isPalindrome(input);
   var expected = true;
   assert.equal(result, expected);
 });
+
+QUnit.load(); // Spustit testy.
