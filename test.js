152c721f132e044a4869828326f6eaf70cd684a5
monika-majorova <39841608+monika-majorova@users.noreply.github.com>
2018-06-03 00:50:33 +0200

    Oprava testu

--- test.js
+++ test.js
@@ -3,16 +3,12 @@ var test = QUnit.test; // Uložit kopii QUnit.test.
 require('qunit-tap')(QUnit, console.log); // Použít console.log pro výstup testu.
 var isPalindrome = require('./palindrom.js'); // Načást soubor palindrom.js.
 
-function testPalindrome(palindrome, expectedResult) {
-  assert.equal(isPalindrome(palindrome), expectedResult);
-}
-
 test('Testování funkce vyhodnocující, zda jde o palindrom', function(assert) {
-  testPalindrome("aaa", true);
+  assert.equal(isPalindrome("aaa"), true);
 });
 
 test('Testování funkce vyhodnocující, zda jde o palindrom. Tento test by měl selhat', function(assert) {
-  testPalindrome("abc", true);
+  assert.equal(isPalindrome("abc"), true);
 });
 
 QUnit.load(); // Spustit testy.
